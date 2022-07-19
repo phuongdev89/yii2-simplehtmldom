1012cea49406f7749d3ed7b24ee71cb6d27c9790
Jennifer Melzer <phuong17889@gmail.com>
2022-07-19 11:07:38 +0700

    Update SimpleHTMLDom.php

--- src/SimpleHTMLDom.php
+++ src/SimpleHTMLDom.php
@@ -103,6 +103,7 @@ class SimpleHTMLDom extends Inflector
      */
     public static function get_web_page($url)
     {
+        extract(parse_url($url));
         $cookie = \Yii::getAlias('@runtime/' . $host . '.txt');
         $user_agent = 'Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
         $options = [
